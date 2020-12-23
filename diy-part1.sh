7c96e7e7e5d9ef55f85530b0f8dce0f26ebbb730
eggydutch <59375032+eggydutch@users.noreply.github.com>
2020-12-24 04:25:07 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -33,14 +33,14 @@ git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon
 rm -rf ../lean/luci-theme-argon
 
 # Add VSSR(HelloWorld)
-git clone https://github.com/jerrykuku/lua-maxminddb.git
-git clone https://github.com/jerrykuku/luci-app-vssr.git
-#cd luci-app-vssr/root/etc/vssr/
-#echo 'china_ssr.txt
-#config/black.list
-#config/white.list
-#dnsmasq.oversea/oversea_list.conf
-#dnsmasq.ssr/ad.conf
-#dnsmasq.ssr/gfw_base.conf
-#dnsmasq.ssr/gfw_list.conf' | xargs rm
-#cd ../../../
+git clone https://github.com/jerrykuku/lua-maxminddb
+git clone https://github.com/jerrykuku/luci-app-vssr
+cd luci-app-vssr/root/etc/vssr/
+echo 'china_ssr.txt
+config/black.list
+config/white.list
+dnsmasq.oversea/oversea_list.conf
+dnsmasq.ssr/ad.conf
+dnsmasq.ssr/gfw_base.conf
+dnsmasq.ssr/gfw_list.conf' | xargs rm
+cd ../../../
