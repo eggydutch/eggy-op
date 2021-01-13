26667b66cfec00d7ecf0d3a589d77b9d52c644ef
eggydutch <59375032+eggydutch@users.noreply.github.com>
2021-01-14 02:56:10 +0800

    Update Lienol.sh

--- Lienol.sh
+++ Lienol.sh
@@ -14,7 +14,7 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
 # Add luci-app-ssr-plus
 #pushd package/lean
