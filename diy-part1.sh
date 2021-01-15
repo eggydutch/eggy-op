6d8cd7a4b41f82b2925a83992cfb4af036206fb6
eggydutch <59375032+eggydutch@users.noreply.github.com>
2021-01-15 19:51:24 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,19 +14,19 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
 # Add luci-app-ssr-plus
-#pushd package/lean
-#git clone --depth=1 https://github.com/fw876/helloworld
-#popd
+pushd package/lean
+git clone --depth=1 https://github.com/fw876/helloworld
+popd
 
 # Add Passwall
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall
 
 # Clone community packages to package/community
-#mkdir package/community
-#pushd package/community
+mkdir package/community
+pushd package/community
 
 # Add OpenClash
 git clone --depth=1 -b master https://github.com/vernesong/OpenClash
@@ -38,18 +38,6 @@ rm -rf ../lean/luci-theme-argon
 # Add cpufreq
 #git clone https://github.com/coolsnowwolf/lede/tree/master/package/lean/luci-app-cpufreq
 
-# Add Diskman
-#git clone https://github.com/lisaac/luci-app-diskman
-
 # Add VSSR(Hello World)
-git clone https://github.com/jerrykuku/lua-maxminddb
-git clone https://github.com/jerrykuku/luci-app-vssr
-#cd luci-app-vssr/root/etc/vssr/
-#echo 'china_ssr.txt
-#config/black.list
-#config/white.list
-#dnsmasq.oversea/oversea_list.conf
-#dnsmasq.ssr/ad.conf
-#dnsmasq.ssr/gfw_base.conf
-#dnsmasq.ssr/gfw_list.conf' | xargs rm
-#cd ../../../
+#git clone https://github.com/jerrykuku/lua-maxminddb
+#git clone https://github.com/jerrykuku/luci-app-vssr
