954951ab565217226d295f8b8d2bb6aa6b6c37b7
eggydutch <59375032+eggydutch@users.noreply.github.com>
2021-01-17 05:47:24 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,20 +13,26 @@
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-# Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-
 # Add luci-app-ssr-plus
 pushd package/lean
 git clone --depth=1 https://github.com/fw876/helloworld
 popd
 
-# Add Passwall
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall
-
 # Clone community packages to package/community
 mkdir package/community
 pushd package/community
+
+# Add Lienol's Packages
+git clone --depth=1 https://github.com/Lienol/openwrt-package
+
+# Use Lienol's https-dns-proxy package
+pushd feeds/packages/net
+rm -rf https-dns-proxy
+svn co https://github.com/Lienol/openwrt-packages/trunk/net/https-dns-proxy
+popd
+
+# Add Passwall
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall
 
 # Add OpenClash
 git clone --depth=1 -b master https://github.com/vernesong/OpenClash
