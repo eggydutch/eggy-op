dddfd282de8a6bace6e4b793ef6ba68007838ac6
eggydutch <59375032+eggydutch@users.noreply.github.com>
2021-02-05 04:15:26 +0800

    Update project-op-p1.sh

--- project-op-p1.sh
+++ project-op-p1.sh
@@ -17,16 +17,16 @@
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
 # Add luci-app-ssr-plus
-pushd package/lean
-git clone --depth=1 https://github.com/fw876/helloworld
-popd
+#pushd package/lean
+#git clone --depth=1 https://github.com/fw876/helloworld
+#popd
 
 # Add Passwall
 #git clone --depth=1 -b main https://github.com/xiaorouji/openwrt-passwall
 
 # Clone community packages to package/community
-mkdir package/community
-pushd package/community
+#mkdir package/community
+#pushd package/community
 
 # Add OpenClash
 #git clone --depth=1 -b master https://github.com/vernesong/OpenClash
