bf6b3f40aa8f26d9a14d0dbb8780d5db3ea4788e
eggydutch <59375032+eggydutch@users.noreply.github.com>
2022-04-17 18:16:55 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,8 +14,8 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add luci-app-ssr-plus
-#pushd package/lean
-#git clone --depth=1 https://github.com/fw876/helloworld
+pushd package/lean
+git clone --depth=1 https://github.com/fw876/helloworld
 #popd
 #sed -i "/helloworld/d" "feeds.conf.default"
 #echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
@@ -49,8 +49,8 @@ git clone --depth=1 -b master https://github.com/vernesong/OpenClash
 #git clone --depth=1 -b openwrt-18.06-k5.4 https://github.com/immortalwrt/immortalwrt/tree/openwrt-18.06-k5.4/package/lean/luci-app-cpufreq
 
 # Add VSSR(Hello World)
-#git clone https://github.com/jerrykuku/lua-maxminddb
-#git clone https://github.com/jerrykuku/luci-app-vssr
+git clone https://github.com/jerrykuku/lua-maxminddb
+git clone https://github.com/jerrykuku/luci-app-vssr
 
 # Add Luci App For Trojan-Go
 #git clone --depth=1 -b master https://github.com/frainzy1477/luci-app-trojan
