d9443beb47bc499b27c1422300bc2e58583ca8f3
eggydutch <59375032+eggydutch@users.noreply.github.com>
2022-07-12 03:19:20 +0800

    Update diy-part2.sh

--- custom/gl.inet/diy-part2.sh
+++ custom/gl.inet/diy-part2.sh
@@ -22,6 +22,18 @@
 #sed -i "/passwall/d" "feeds.conf.default"
 #echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall" >> "feeds.conf.default"
 
-# Openclash
-#sed -i "/openclash/d" "feeds.conf.default"
-#echo "src-git openclash https://github.com/vernesong/OpenClash" >> "feeds.conf.default"
+# Add Lienol's packages
+sed -i "/lienol/d" "feeds.conf.default"
+echo "src-git lienol https://github.com/Lienol/openwrt-package" >> "feeds.conf.default"
+
+# Add Kenzok8 packages
+sed -i "/kenzok8/d" "feeds.conf.default"
+echo "src-git kenzok8 https://github.com/kenzok8/openwrt-packages" >> "feeds.conf.default"
+
+# Add Kenzok8 small packages
+sed -i "/kenzok8small/d" "feeds.conf.default"
+echo "src-git kenzok8small https://github.com/kenzok8/small" >> "feeds.conf.default"
+
+# Add Immortalwrt packages
+sed -i "/immortalwrt/d" "feeds.conf.default"
+echo "src-git immortalwrt https://github.com/immortalwrt/luci/tree/master/applications" >> "feeds.conf.default"
