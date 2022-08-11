3c52a5aef8b52122d82b263a74d680b6e1ebd6d7
eggydutch <59375032+eggydutch@users.noreply.github.com>
2022-08-11 19:11:38 +0800

    Update diy-part2.sh

--- test/gl.inet/diy-part2.sh
+++ test/gl.inet/diy-part2.sh
@@ -14,6 +14,10 @@
 # sed -i 's/192.168.1.1/192.168.1.10/g' package/base-files/files/bin/config_generate
 # echo -e '\nEggy Build\n' >> package/base-files/files/etc/banner
 
+# Add NueXini Packages
+sed -i "/NueXini/d" "feeds.conf.default"
+echo "src-git NueXini https://github.com/NueXini/NueXini_Packages" >> "feeds.conf.default"
+
 # SSRP+
 #sed -i "/ssrplus/d" "feeds.conf.default"
 #echo "src-git ssrplus https://github.com/fw876/helloworld" >> "feeds.conf.default"
