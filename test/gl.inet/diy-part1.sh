5838f53e2b431ab8a801818970800a84f126660d
eggydutch <59375032+eggydutch@users.noreply.github.com>
2023-03-09 04:45:04 +0800

    Update diy-part1.sh

--- test/gl.inet/diy-part1.sh
+++ test/gl.inet/diy-part1.sh
@@ -11,14 +11,14 @@
 #
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add luci-app-ssr-plus
-#pushd package/lean
-#git clone --depth=1 https://github.com/fw876/helloworld
-#popd
-#sed -i "/helloworld/d" "feeds.conf.default"
-#echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
+pushd package/lean
+git clone --depth=1 https://github.com/fw876/helloworld
+popd
+sed -i "/helloworld/d" "feeds.conf.default"
+echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
 
 # Clone community packages to package/community
 #mkdir package/community
@@ -49,7 +49,7 @@
 #git clone --depth=1 -b main https://github.com/xiaorouji/openwrt-passwall2
 
 # Add OpenClash
-#git clone --depth=1 -b master https://github.com/vernesong/OpenClash
+git clone --depth=1 -b master https://github.com/vernesong/OpenClash
 
 # Add luci-theme-argon
 #git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon
@@ -60,8 +60,8 @@
 #git clone --depth=1 -b openwrt-18.06-k5.4 https://github.com/immortalwrt/immortalwrt/tree/openwrt-18.06-k5.4/package/lean/luci-app-cpufreq
 
 # Add VSSR(Hello World)
-#git clone https://github.com/jerrykuku/lua-maxminddb
-#git clone https://github.com/jerrykuku/luci-app-vssr
+git clone https://github.com/jerrykuku/lua-maxminddb
+git clone https://github.com/jerrykuku/luci-app-vssr
 
 # Add Luci App For Trojan-Go
 #git clone --depth=1 -b master https://github.com/frainzy1477/luci-app-trojan
