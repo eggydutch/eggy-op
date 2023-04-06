914ee0fd653de2d5a5a5e88fea336629ff457513
eggydutch <59375032+eggydutch@users.noreply.github.com>
2023-04-06 19:21:48 +0800

    Update diy-part1.sh

--- custom/gl.inet/diy-part1.sh
+++ custom/gl.inet/diy-part1.sh
@@ -14,7 +14,7 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add luci-app-ssr-plus
-pushd package/lean
+#pushd package/lean
 #git clone --depth=1 https://github.com/fw876/helloworld
 #popd
 #sed -i "/helloworld/d" "feeds.conf.default"
@@ -25,7 +25,7 @@ pushd package/lean
 #pushd package/community
 
 # Add NueXini Packages
-git clone https://github.com/NueXini/NueXini_Packages
+#git clone https://github.com/NueXini/NueXini_Packages
 
 # Add Immortalwrt Packages
 #git clone https://github.com/immortalwrt/packages
@@ -44,12 +44,12 @@ git clone https://github.com/NueXini/NueXini_Packages
 #git clone https://github.com/immortalwrt/luci/tree/openwrt-21.02/applications
 
 # Add Passwall
-git clone --depth=1 -b packages https://github.com/xiaorouji/openwrt-passwall
+#git clone --depth=1 -b packages https://github.com/xiaorouji/openwrt-passwall
 #git clone --depth=1 -b luci https://github.com/xiaorouji/openwrt-passwall
 #git clone --depth=1 -b main https://github.com/xiaorouji/openwrt-passwall2
 
 # Add OpenClash
-git clone --depth=1 -b master https://github.com/vernesong/OpenClash
+#git clone --depth=1 -b master https://github.com/vernesong/OpenClash
 
 # Add luci-theme-argon
 #git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon
@@ -60,8 +60,8 @@ git clone --depth=1 -b master https://github.com/vernesong/OpenClash
 #git clone --depth=1 -b openwrt-18.06-k5.4 https://github.com/immortalwrt/immortalwrt/tree/openwrt-18.06-k5.4/package/lean/luci-app-cpufreq
 
 # Add VSSR(Hello World)
-git clone https://github.com/jerrykuku/lua-maxminddb
-git clone https://github.com/jerrykuku/luci-app-vssr
+#git clone https://github.com/jerrykuku/lua-maxminddb
+#git clone https://github.com/jerrykuku/luci-app-vssr
 
 # Add Luci App For Trojan-Go
 #git clone --depth=1 -b master https://github.com/frainzy1477/luci-app-trojan
