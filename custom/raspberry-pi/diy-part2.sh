fb1505c52e561018afd6a1fd7bfba9160a76bb22
eggydutch <59375032+eggydutch@users.noreply.github.com>
2024-02-12 23:26:25 +0800

    Update diy-part2.sh

--- custom/raspberry-pi/diy-part2.sh
+++ custom/raspberry-pi/diy-part2.sh
@@ -12,9 +12,10 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.1.1/g' package/base-files/files/bin/config_generate
-# rm -rf feeds/luci/applications/luci-app-openclash
-# rm -rf feeds/packages/net/xray-core/
-# rm -rf feeds/packages/net/xray-plugin/
+cd openwrt && rm -rf ./feeds/luci/applications/luci-app-openclash
+cd openwrt && rm -rf ./feeds/packages/net/luci-app-passwall
+cd openwrt && rm -rf ./feeds/packages/net/xray-core
+cd openwrt && rm -rf ./feeds/packages/net/xray-plugin
 git clone https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
 git clone https://github.com/xiaorouji/openwrt-passwall-packages package/luci-app-passwall-packages
 
