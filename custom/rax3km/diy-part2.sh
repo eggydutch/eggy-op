ec68f0faecabf13f3235ec6830cbfa26a9f7d9d6
eggydutch <59375032+eggydutch@users.noreply.github.com>
2024-02-14 05:25:08 +0800

    Update diy-part2.sh

--- custom/rax3km/diy-part2.sh
+++ custom/rax3km/diy-part2.sh
@@ -12,10 +12,13 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.1.1/g' package/base-files/files/bin/config_generate
-# rm -rf feeds/luci/applications/openclash/
-# rm -rf feeds/packages/net/xray-core/
-# rm -rf feeds/packages/net/xray-plugin/
-# git clone https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
-# git clone https://github.com/xiaorouji/openwrt-passwall-packages package/luci-app-passwall-packages
 
+# Remove old version packages
+rm -rf feeds/luci/applications/openclash/
+rm -rf feeds/luci/applications/luci-app-passwall/
+rm -rf feeds/packages/net/xray-core/
+rm -rf feeds/packages/net/xray-plugin/
+
+# Update Golang
+cd openwrt && git clone -b master --single-branch https://github.com/immortalwrt/packages.git packages_master && rm -rf ./feeds/packages/lang/golang && mv ./packages_master/lang/golang ./feeds/packages/lang && rm -rf ./packages_master
 #echo -e '\nEggy Build\n' >> package/base-files/files/etc/banner
