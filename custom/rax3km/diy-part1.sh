d684c1a54ebaa2b006b3197ddc3871f6fc201bce
eggydutch <59375032+eggydutch@users.noreply.github.com>
2024-02-17 01:01:21 +0800

    Update diy-part1.sh

--- custom/rax3km/diy-part1.sh
+++ custom/rax3km/diy-part1.sh
@@ -16,6 +16,8 @@
 # Add a feed source
 # echo 'src-git kiddin9 https://github.com/kiddin9/openwrt-packages' >>feeds.conf.default
 # git clone https://github.com/kiddin9/openwrt-packages package/kiddin9
+# sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+# sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 # echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
