2b7199af96c6ff4d46e45c8714f31c9b7421d5c5
eggydutch <59375032+eggydutch@users.noreply.github.com>
2024-02-17 20:07:28 +0800

    Update diy-part2.sh

--- test/rax3km/diy-part2.sh
+++ test/rax3km/diy-part2.sh
@@ -14,15 +14,16 @@
 sed -i 's/192.168.1.1/192.168.1.1/g' package/base-files/files/bin/config_generate
 
 # Remove old version packages
-rm -rf ./feeds/luci/applications/openclash
-rm -rf ./feeds/luci/applications/luci-app-ssr-plus
-# rm -rf ./feeds/luci/applications/luci-app-passwall
-rm -rf ./feeds/packages/net/xray-core
-rm -rf ./feeds/packages/net/xray-plugin
+rm -rf /feeds/luci/applications/openclash
+# rm -rf /feeds/luci/applications/luci-app-ssr-plus
+# rm -rf /feeds/luci/applications/luci-app-passwall
+rm -rf /feeds/packages/net/xray-core
+rm -rf /feeds/packages/net/xray-plugin
 git clone -b master --single-branch https://github.com/fw876/helloworld.git package/luci-app-ssr-plus
 git clone -b master --single-branch https://github.com/vernesong/OpenClash.git package/openclash
 # git clone -b main --single-branch https://github.com/xiaorouji/openwrt-passwall.git package/luci-app-passwall
-# git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall_packages
+git clone -b main --single-branch https://github.com/xiaorouji/openwrt-passwall2.git package/luci-app-passwall2
+git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall_packages
 # rm -rf $(find ./feeds/luci/ -type d -regex ".*\(passwall\|openclash\).*")
 # rm -rf $(find ./feeds/packages/net/ -type d -regex ".*\(xray-core\|xray-plugin\).*")
 # rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd-alt,miniupnpd-iptables,wireless-regdb}
