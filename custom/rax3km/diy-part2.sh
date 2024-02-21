daba498f3088e25db874b4563f1768bae8188b59
eggydutch <59375032+eggydutch@users.noreply.github.com>
2024-02-21 17:32:06 +0800

    更新 diy-part2.sh

--- custom/rax3km/diy-part2.sh
+++ custom/rax3km/diy-part2.sh
@@ -19,6 +19,9 @@ sed -i 's/192.168.1.1/192.168.1.1/g' package/base-files/files/bin/config_generat
 # rm -rf ./feeds/luci/applications/luci-app-passwall
 # rm -rf ./feeds/packages/net/xray-core
 # rm -rf ./feeds/packages/net/xray-plugin
+# rm -zrf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd-alt,miniupnpd-iptables,wireless-regdb}
+
+# Update packages
 git clone -b master --single-branch https://github.com/fw876/helloworld.git package/luci-app-ssr-plus
 git clone -b master --single-branch https://github.com/vernesong/OpenClash.git package/openclash
 # git clone -b main --single-branch https://github.com/xiaorouji/openwrt-passwall.git package/luci-app-passwall
@@ -26,4 +29,3 @@ git clone -b main --single-branch https://github.com/xiaorouji/openwrt-passwall2
 git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall_packages
 # git clone -b master --single-branch https://github.com/immortalwrt/immortalwrt.git package/ip_master
 
-# rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd-alt,miniupnpd-iptables,wireless-regdb}
