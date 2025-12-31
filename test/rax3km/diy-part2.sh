9376c5dff810e65e1f01281e72fa848a098c2668
eggydutch <59375032+eggydutch@users.noreply.github.com>
2026-01-01 03:52:41 +0800

    Comment out package cloning commands in diy-part2.sh
    Comment out several git clone commands for packages.

--- test/rax3km/diy-part2.sh
+++ test/rax3km/diy-part2.sh
@@ -14,10 +14,10 @@
 sed -i 's/192.168.1.1/192.168.1.1/g' package/base-files/files/bin/config_generate
 
 # Update packages
-git clone -b master --single-branch https://github.com/fw876/helloworld.git package/luci-app-ssr-plus
+# git clone -b master --single-branch https://github.com/fw876/helloworld.git package/luci-app-ssr-plus
 git clone -b master --single-branch https://github.com/vernesong/OpenClash.git package/openclash
 # git clone -b main --single-branch https://github.com/xiaorouji/openwrt-passwall.git package/luci-app-passwall
-git clone -b main --single-branch https://github.com/xiaorouji/openwrt-passwall2 package/luci-app-passwall2
-git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall_packages
+# git clone -b main --single-branch https://github.com/xiaorouji/openwrt-passwall2 package/luci-app-passwall2
+# git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall_packages
 # git clone -b master --single-branch https://github.com/immortalwrt/packages.git packages_master && rm -rf ./feeds/packages/lang/golang && mv ./packages_master/lang/golang ./feeds/packages/lang && rm -rf ./packages_master
 #echo -e '\nEggy Build\n' >> package/base-files/files/etc/banner
