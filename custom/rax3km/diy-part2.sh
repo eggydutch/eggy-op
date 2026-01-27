f9e2597a4a4522cad831e51056ff1640df69ad77
eggydutch <59375032+eggydutch@users.noreply.github.com>
2026-01-27 19:31:50 +0800

    Update diy-part2.sh

--- custom/rax3km/diy-part2.sh
+++ custom/rax3km/diy-part2.sh
@@ -24,7 +24,6 @@ sed -i 's/192.168.1.1/192.168.1.1/g' package/base-files/files/bin/config_generat
 # Update packages
 # git clone -b master --single-branch https://github.com/fw876/helloworld.git package/luci-app-ssr-plus
 git clone -b master --single-branch https://github.com/vernesong/OpenClash.git package/openclash
-# git clone -b main --single-branch https://github.com/xiaorouji/openwrt-passwall.git package/luci-app-passwall
 git clone -b main --single-branch https://github.com/Openwrt-Passwall/openwrt-passwall2 package/luci-app-passwall2
 git clone https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git package/passwall_packages
 git clone -b main --single-branch https://github.com/sbwml/luci-app-alist.git package/luci-app-alist
